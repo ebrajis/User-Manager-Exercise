ca348f5f4208970c7046f21d58e0f92f40d1274b
Evaldas Brajis <brajis.evaldas@gmail.com>
2021-07-28 10:59:25 +0300

    create first Method handleSubmit. con.log.

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -12,18 +12,26 @@ class UserForm extends Component {
     },
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('stop right there');
+  };
+
   render() {
     return (
       <div className="user-form">
         <h1>Vartotojo duomenų forma</h1>
         <div className="d-flex">
-          <form>
+          <form onSubmit={this.handleSubmit}></form>
             <input className="input" type="text" name="username" placeholder="Vartotojo vardas" />
             <input className="input" type="text" name="email" placeholder="El.Paštas" />
             <input className="input" type="text" name="password" placeholder="Slaptažodis" />
             <input className="input" type="text" name="repeatPassword" placeholder="Pakartoti Slaptažodi" />
             <input className="input" type="text" name="age" placeholder="Amžius" />
-            <button type="submit">Siųsti</button>
+            <button type="submit">Sukurti</button>
+            <button type="submit">Ištrinti</button>
+            <button type="submit">Redaguoti</button>
+            <button type="submit">Peržiūrėti</button>
           </form>
         </div>
       </div>
