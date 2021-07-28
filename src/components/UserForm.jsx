bb220d97840bf3b3b7736178f1cfecceceaf4e24
Evaldas Brajis <brajis.evaldas@gmail.com>
2021-07-28 11:13:36 +0300

    add handleInput ant put all methods on inputs

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 
 class UserForm extends Component {
   state = {
-    account: {
-      username: '',
-      email: '',
-      password: '',
-      repeatPassword: '',
-      age: '',
-      agreement: false,
-    },
+    username: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+    age: '',
+    agreement: false,
   };
 
   handleSubmit = (e) => {
@@ -17,21 +15,63 @@ class UserForm extends Component {
     console.log('stop right there');
   };
 
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { state: s } = this;
     return (
       <div className="user-form">
         <h1>Vartotojo duomenų forma</h1>
         <div className="d-flex">
-          <form onSubmit={this.handleSubmit}></form>
-            <input className="input" type="text" name="username" placeholder="Vartotojo vardas" />
-            <input className="input" type="text" name="email" placeholder="El.Paštas" />
-            <input className="input" type="text" name="password" placeholder="Slaptažodis" />
-            <input className="input" type="text" name="repeatPassword" placeholder="Pakartoti Slaptažodi" />
-            <input className="input" type="text" name="age" placeholder="Amžius" />
-            <button type="submit">Sukurti</button>
-            <button type="submit">Ištrinti</button>
-            <button type="submit">Redaguoti</button>
-            <button type="submit">Peržiūrėti</button>
+          <form onSubmit={this.handleSubmit} autoComplete="off">
+            <input
+              value={s.username}
+              onChange={this.handleInput}
+              className="input"
+              type="text"
+              name="username"
+              placeholder="Vartotojo vardas"
+            />
+            <input
+              value={s.email}
+              onChange={this.handleInput}
+              className="input"
+              type="text"
+              name="email"
+              placeholder="El.Paštas"
+            />
+            <input
+              value={s.password}
+              onChange={this.handleInput}
+              className="input"
+              type="text"
+              name="password"
+              placeholder="Slaptažodis"
+            />
+            <input
+              value={s.repeatPassword}
+              onChange={this.handleInput}
+              className="input"
+              type="text"
+              name="repeatPassword"
+              placeholder="Pakartoti Slaptažodi"
+            />
+            <input
+              value={s.age}
+              onChange={this.handleInput}
+              className="input"
+              type="text"
+              name="age"
+              placeholder="Amžius"
+            />
+            <div className="buttons">
+              <button type="submit">Sukurti</button>
+              <button type="submit">Ištrinti</button>
+              <button type="submit">Redaguoti</button>
+              <button type="submit">Peržiūrėti</button>
+            </div>
           </form>
         </div>
       </div>
