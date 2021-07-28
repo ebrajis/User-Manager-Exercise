2db205e27d5a1be1d9a1b8a2e8777561ac9cabb5
Evaldas Brajis <brajis.evaldas@gmail.com>
2021-07-28 10:50:17 +0300

    Add inputs to userForm and style

--- README.md
+++ README.md
@@ -13,3 +13,34 @@
 * Create new folder in src directory => Components and inside create new file UserForm.jsx
 
 * Fill the app.jsx file/ import UserForm.jsx and type to class render.
+
+# This is full stack user-manager application to create user form
+
+A simple app to create, update, delete users
+
+## instalation
+
+1. `npm install`
+2. copy .env.sample to .env and add your mongo db string
+
+## scripts
+
+1. `npm start` runs frontend.
+2. `npm run back` runs backend with nodemon
+
+## Dependancies
+
+### backend side
+
+1. "cors"
+1. "dotenv"
+1. "express"
+1. "mongoose"
+1. "morgan"
+
+### frontend side
+
+1. "axios"
+1. "bootstrap"
+1. "react"
+1. "react-dom"
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -8,11 +8,26 @@ class UserForm extends Component {
       password: '',
       repeatPassword: '',
       age: '',
+      agreement: false,
     },
   };
 
   render() {
-    return <div className="user-form">UserForm</div>;
+    return (
+      <div className="user-form">
+        <h1>Vartotojo duomenų forma</h1>
+        <div className="d-flex">
+          <form>
+            <input className="input" type="text" name="username" placeholder="Vartotojo vardas" />
+            <input className="input" type="text" name="email" placeholder="El.Paštas" />
+            <input className="input" type="text" name="password" placeholder="Slaptažodis" />
+            <input className="input" type="text" name="repeatPassword" placeholder="Pakartoti Slaptažodi" />
+            <input className="input" type="text" name="age" placeholder="Amžius" />
+            <button type="submit">Siųsti</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
