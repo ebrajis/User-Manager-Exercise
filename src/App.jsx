ffded8d700e867ebbaa3cde0849502f7b4703e1a
Evaldas Brajis <brajis.evaldas@gmail.com>
2021-07-28 11:33:45 +0300

    import axios, axios.post trycatch block.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import UserForm from './components/UserForm';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {};
-  createNewUser = (dataToCreateNewUser) => {
+  createNewUser = async (dataToCreateNewUser) => {
     console.log('createNewUser in app.jsx');
     // console.log('dataToCreateNewUser', dataToCreateNewUser);
+    try {
+      const createResult = await axios.post('http://localhost:3000/api/user/new', dataToCreateNewUser);
+      console.log('createResult', createResult);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   render() {
     return (
       <div className="App">
